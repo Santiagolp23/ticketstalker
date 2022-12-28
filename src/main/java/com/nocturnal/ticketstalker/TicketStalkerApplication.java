70a706b4c4d7e340d538cbea777ba4a899bbc9f7
Santiago Lopez <santiagolopezpardo@gmail.com>
2022-12-28 17:52:59 -0500

    initial commit fo spring boot project

---
+++ src/main/java/com/nocturnal/ticketstalker/TicketStalkerApplication.java
@@ -0,0 +1,13 @@
+package com.nocturnal.ticketstalker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketStalkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketStalkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nocturnal/ticketstalker/TicketStalkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocturnal.ticketstalker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketStalkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
