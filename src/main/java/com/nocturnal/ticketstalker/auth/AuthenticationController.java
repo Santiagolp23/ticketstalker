d3bba72495f045395922d10ed0e8557194593f32
Santiago Lopez <santiagolopezpardo@gmail.com>
2023-02-25 19:23:30 -0500

    fixed constructor for controller, final field was not initizialized

--- src/main/java/com/nocturnal/ticketstalker/auth/AuthenticationController.java
+++ src/main/java/com/nocturnal/ticketstalker/auth/AuthenticationController.java
@@ -1,11 +1,13 @@
 package com.nocturnal.ticketstalker.auth;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthenticationController {
